4d4046e913a056b17dfc5f2556813efef7535f6b
NguyenHongBan <hongbanng021002@gmail.com>
2024-01-08 22:35:12 +0700

    update

--- BanHangOnline/Models/CategoriesModel.cs
+++ BanHangOnline/Models/CategoriesModel.cs
@@ -1,7 +1,18 @@
-﻿namespace BanHangOnline.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BanHangOnline.Models
 {
 	public class CategoriesModel
 	{
+		[Key]
 		public int Id { get; set; }
+		[Required, MinLength(4, ErrorMessage = "Yêu cầu nhập Tên Danh mục")]
+		public string Name { get; set; }
+		[Required, MinLength(4, ErrorMessage = "Yêu cầu nhập mô tả Danh mục")]
+		public string Description { get; set; }
+		[Required]
+		public string Slug { get; set; }
+
+		public int Status { get; set; }
 	}
 }
