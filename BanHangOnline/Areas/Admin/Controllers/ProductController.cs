1ef874001f00f779467e8708f9b03e636ca37f2c
NguyenHongBan <hongbanng021002@gmail.com>
2024-01-12 15:41:58 +0700

    update

---
+++ BanHangOnline/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,19 @@
+﻿using BanHangOnline.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BanHangOnline.Areas.Admin.Controllers
+{
+	[Area("Admin")]
+	public class ProductController : Controller
+	{
+		private readonly DataContext _dataContext;
+		public ProductController(DataContext context)
+		{
+			_dataContext = context;
+		}
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
