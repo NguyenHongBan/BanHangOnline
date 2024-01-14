dda0edba632410c1b9c11902cde70d6153887a04
NguyenHongBan <hongbanng021002@gmail.com>
2024-01-14 14:55:33 +0700

    update2h54

---
+++ BanHangOnline/Areas/Admin/Controllers/BrandsController.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BanHangOnline.Areas.Admin.Controllers
+{
+	public class BrandsController : Controller
+	{
+	}
+}
---
+++ BanHangOnline/Areas/Admin/Controllers/CategoriesController.cs
@@ -0,0 +1,34 @@
+﻿using BanHangOnline.Repository;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BanHangOnline.Areas.Admin.Controllers
+{
+	[Area("Admin")]
+	public class CategoriesController : Controller
+	{
+		private readonly DataContext _dataContext;
+
+		public CategoriesController(DataContext context)
+		{
+			_dataContext = context;
+			
+		}
+
+		public async Task<IActionResult> Index()
+		{
+			return View(await _dataContext.Categories.OrderByDescending(c => c.Id).ToListAsync());
+		}
+
+		public async Task<IActionResult> Search(string searchString)
+		{
+
+			var products = await _dataContext.Categories
+				.Where(p => p.Name.Contains(searchString) || p.Description.Contains(searchString))
+				.ToListAsync();
+
+			return View("Index", products);
+		}
+	}
+}
