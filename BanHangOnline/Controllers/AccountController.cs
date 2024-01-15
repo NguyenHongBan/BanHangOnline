8959c5eb11a88818ed786889bc53e7ce610ac684
NguyenHongBan <hongbanng021002@gmail.com>
2024-01-16 01:52:28 +0700

    update 1h51

---
+++ BanHangOnline/Controllers/AccountController.cs
@@ -0,0 +1,58 @@
+﻿using BanHangOnline.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BanHangOnline.Controllers
+{
+    public class AccountController : Controller
+    {
+        private UserManager<AppUserModel> _userManage;
+		private SignInManager<AppUserModel> _signInManage;
+
+        public AccountController(SignInManager<AppUserModel> signInManager, UserManager<AppUserModel> userManager)
+        {
+            _signInManage = signInManager;
+            _userManage = userManager;
+        }
+		public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Login()
+        {
+            return View();
+        }
+
+		public IActionResult Create()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Create(UserModel user)
+		{
+            if(ModelState.IsValid)
+            {
+                AppUserModel newUser = new AppUserModel 
+                { 
+                    UserName = user.Username,
+                    Email = user.Email,
+                };
+                IdentityResult result = await _userManage.CreateAsync(newUser);
+                if(result.Succeeded)
+                {
+                    TempData["success"] = "Đăng ký thành công!";
+                    return Redirect("/account");
+                }
+                foreach(IdentityError error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+            }
+			return View(user);
+		}
+
+	}
+}
--- BanHangOnline/Program.cs
+++ BanHangOnline/Program.cs
@@ -1,4 +1,6 @@
+using BanHangOnline.Models;
 using BanHangOnline.Repository;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,7 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Connection database
 builder.Services.AddDbContext<DataContext>(options =>
 {
-    options.UseSqlServer(builder.Configuration["ConnectionStrings:BanHangOnline"]);
+	options.UseSqlServer(builder.Configuration["ConnectionStrings:BanHangOnline"]);
 });
 
 // Add services to the container.
@@ -16,8 +18,27 @@ builder.Services.AddDistributedMemoryCache();
 
 builder.Services.AddSession(options =>
 {
-    options.IdleTimeout = TimeSpan.FromMinutes(10);
-    options.Cookie.IsEssential = true;
+	options.IdleTimeout = TimeSpan.FromMinutes(10);
+	options.Cookie.IsEssential = true;
+});
+
+builder.Services.AddIdentity<AppUserModel, IdentityRole>(options =>
+{
+	options.User.RequireUniqueEmail = false;
+})
+	.AddEntityFrameworkStores<DataContext>()
+	.AddDefaultTokenProviders();
+
+builder.Services.Configure<IdentityOptions>(options =>
+{
+	// Password settings.
+	options.Password.RequireDigit = true;
+	options.Password.RequireLowercase = true;
+	options.Password.RequireNonAlphanumeric = false;
+	options.Password.RequireUppercase = false;
+	options.Password.RequiredLength = 4;
+
+	options.User.RequireUniqueEmail = false;
 });
 
 var app = builder.Build();
@@ -27,35 +48,36 @@ app.UseSession();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
+	app.UseExceptionHandler("/Home/Error");
 }
 app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
-    name: "Areas",
-    pattern: "{area:exists}/{controller=Product}/{action=Index}/{id?}");
+	name: "Areas",
+	pattern: "{area:exists}/{controller=Product}/{action=Index}/{id?}");
 
 app.MapControllerRoute(
-    name: "Categories",
-    pattern: "/Categories/{Slug?}",
-    defaults: new { controller = "Categories", Action = "Index" });
+	name: "Categories",
+	pattern: "/Categories/{Slug?}",
+	defaults: new { controller = "Categories", Action = "Index" });
 
 app.MapControllerRoute(
-    name: "Brands",
-    pattern: "/Brands/{Slug?}",
-    defaults: new { controller = "Brands", Action = "Index" });
+	name: "Brands",
+	pattern: "/Brands/{Slug?}",
+	defaults: new { controller = "Brands", Action = "Index" });
 
 app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+	name: "default",
+	pattern: "{controller=Home}/{action=Index}/{id?}");
 
 
 //seeding data
-var context = app.Services.CreateScope().ServiceProvider.GetRequiredService<DataContext>(); 
+var context = app.Services.CreateScope().ServiceProvider.GetRequiredService<DataContext>();
 SeedData.SeedingData(context);
 
 app.Run();
